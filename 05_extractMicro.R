514797bafc7ea9535dd95062f3cff17d0f894f4f
Xiaqu Zhou <u0142858@kuleuven.be>
2024-07-31 18:02:20 +0200

    update the scripts. Change to focus on different plots

--- 05_extractMicro.R
+++ 05_extractMicro.R
@@ -1,6 +1,4 @@
 library(terra)
-install.packages("sf")
-install.packages("mapview")
 library(sf)
 library(mapview)
 
@@ -15,50 +13,38 @@ colnames(xy)[1] <- "plotID"
 colnames(xy)[2] <- "latitude"
 colnames(xy)[3] <- "longitude"
 head(xy)
-unique(no_na1995s$plotID)
-eu_076 <- xy[grep("EU_076_", xy$plotID), ]
-eu_078 <- xy[grep("EU_078_", xy$plotID), ]
+#add xy to the deltacit dataset.
+no_na1995s02 <- merge(no_na1995s, xy, by = "plotID")
+no_na1995s02 <- no_na1995s02[, c(1,14,15,2,3,4,5,6,7,8,9,10,11,12,13)]
 
+plots_xy <- no_na1995s02[, c(1:3)]
 
 # Convert data frame to sf object
-eu_082_sf <- st_as_sf(x = eu_082, 
-                      coords = c("latitude", "longitude"),
-                      crs = "+proj=longlat +datum=WGS84")
-
-eu_076_sf <- st_as_sf(x = eu_076, 
+eu_plots_sf <- st_as_sf(x = plots_xy, 
                       coords = c("longitude", "latitude"),
                       crs = "+proj=longlat +datum=WGS84")
 
+mapview(eu_plots_sf)
 
-eu_076_st <- st_transform(eu_076_sf, crs = st_crs(offset))
-eu_082_st <- st_transform(eu_082_sf, crs = st_crs(offset))
+eu_plots_st <- st_transform(eu_plots_sf, crs = st_crs(offset))
 
 # interactive map:
-
-mapview(eu_082_sf)
-mapview(eu_010b_sf)
-mapview(eu_008_st)
-mapview(eu_082_st)
+mapview(eu_plots_st)
 
 # Extract the microclimate values.
-eu_008_st <- vect(eu_008_st)
-e8 <- extract(offset,eu_008_st, method = "simple", bind = T )
-e8_micro <- as.data.frame(e8)
+eu_plots_st <- vect(eu_plots_st)
+plotsmicro <- extract(offset,eu_plots_st, method = "simple", bind = T )
+plotsmicro <- as.data.frame(plotsmicro)
 
-eu_082_st <- vect(eu_082_st)
-e82 <- extract(offset,eu_082_st, method = "simple", bind = T )
-e82_micro <- as.data.frame(e82)
-hist(e82_micro$mean_annualOffset)
-
-# save data
-save(e8_micro, file = "I:/DATA/output/extraMicro/EU_008_MAToffset.RData")
-save(e82_micro, file = "I:/DATA/output/extraMicro/EU_082_MAToffset.RData")
-
-#load data
-load("I:/DATA/output/extraMicro/EU_082_MAToffset.RData")
-load("I:/DATA/output/CITyearly/R1toR2_CITperYR_maxt_Gs_eu082.RData")
+hist(plotsmicro$mean_annualOffset)
 
 # merge micro with CIT changes
-micro_cit <- merge(r1tor2_10yrs, e82_micro, by = "plotID")
-cor.test(micro_cit$mean_annualOffset, micro_cit$CITperYR)
+micro_cit <- merge(no_na1995s, plotsmicro, by = "plotID")
+hist(micro_cit$mean_annualOffset)
+cor.test(micro_cit$mean_annualOffset, micro_cit$cit_perYR1R2, method = "spearman")
+plot(micro_cit$mean_annualOffset, micro_cit$cit_perYbase2R1)
 hist(micro_cit$CITperYR)
+
+# save data
+save(micro_cit, file = "I:/DATA/output/extraMicro/micro_deltaCIT_maxtGs_1995s.RData")
+save(no_na1995s02, file = "I:/DATA/output/CITyearly/Base2Re2_maxt_Gs_1995s.RData")
